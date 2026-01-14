e363464fc6ef6b4d24b6dbbe5c5d75e911ca5706
Kazu5102 <nomusan1010@gmail.com>
2026-01-14 14:53:14 +0900

    Ver3.21bb

--- App.tsx
+++ App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
                 <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
                     <div className="flex items-center gap-2">
                         <span className="bg-rose-500 text-white text-[10px] font-black px-1.5 py-0.5 rounded uppercase tracking-tighter">System</span>
-                        <span className="text-sm font-mono font-bold text-white tracking-wide">Ver 3.21</span>
+                        <span className="text-sm font-mono font-bold text-white tracking-wide">Ver 3.21BBB</span>
                     </div>
 
                     <div className="flex items-center">
